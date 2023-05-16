310f32a37fbc5730314c96905fec8638293042db
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-17 08:59:22 +0900

    spotify で 情報を取得

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"os"
+
+	"fmt"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2/clientcredentials"
+
+	"github.com/zmb3/spotify/v2"
 )
 
 func main() {
-	fmt.Println(os.Getenv("SPOTIFY_CLIENT_ID"))
+	ctx := context.Background()
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_CLIENT_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_CLIENT_SECRET"),
+		TokenURL:     spotifyauth.TokenURL,
+	}
+
+	toke, err := config.Token(ctx)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	httpClient := spotifyauth.New().Client(ctx, toke)
+	client := spotify.New(httpClient)
+
+	// Get a playlist
+	playlist, err := client.GetPlaylist(ctx, "37i9dQZF1DXcBWIGoYBM5M")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(playlist.Name)
 }
